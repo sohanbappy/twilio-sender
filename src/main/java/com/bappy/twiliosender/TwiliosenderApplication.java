c99adf3966e5f3e58c3edde2fde70bf053727875
bappy <bappy@rokomari.com>
2020-10-29 22:27:37 +0600

    initialized

---
+++ src/main/java/com/bappy/twiliosender/TwiliosenderApplication.java
@@ -0,0 +1,13 @@
+package com.bappy.twiliosender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiliosenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiliosenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bappy/twiliosender/TwiliosenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bappy.twiliosender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwiliosenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
